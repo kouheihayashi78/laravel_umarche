1a8b661d28e3713bbd6a9df58c8f1fc08a648f13
kouheihayashi78 <kouhei.basketball7.8@gmail.com>
2022-02-20 02:16:11 +0900

    スロット

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,1 +1,5 @@
+<x-tests.app>
+    <x-slot name="header">ヘッダー1</x-slot>
+    
 コンポーネント1
+</x-tests.app>
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,1 +1,5 @@
+<x-tests.app>
+    <x-slot name="header">ヘッダー2</x-slot>
+    
 コンポーネント2
+</x-tests.app>
