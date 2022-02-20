e934f7d90dfdcc7c16a3091bbae41e478416108e
kouheihayashi78 <kouhei.basketball7.8@gmail.com>
2022-02-20 15:04:43 +0900

    props

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,6 +1,7 @@
 <x-tests.app>
     <x-slot name="header">ヘッダー1</x-slot>
 コンポーネント1
-    <x-tests.card title="タイトル" content="コンテンツ" :message="$message"/>
+    <x-tests.card title="タイトル1" content="コンテンツ" :message="$message"/>
     <!-- 変数の場合は:をつけて書く -->
+    <x-tests.card title="タイトル2" />
 </x-tests.app>
