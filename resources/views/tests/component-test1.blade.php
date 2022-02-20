f506f6b35350b4aa022380696e72ccd46a8b9d75
kouheihayashi78 <kouhei.basketball7.8@gmail.com>
2022-02-20 14:54:32 +0900

    属性と変数

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,5 +1,6 @@
 <x-tests.app>
     <x-slot name="header">ヘッダー1</x-slot>
-    
 コンポーネント1
+    <x-tests.card title="タイトル" content="コンテンツ" :message="$message"/>
+    <!-- 変数の場合は:をつけて書く -->
 </x-tests.app>
